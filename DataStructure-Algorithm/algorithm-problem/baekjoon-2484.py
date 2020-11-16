55b975621f959383acc7e0be8f041b3f9ca72f87
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-16 23:01:55 +0900

    [ADD] DataStructure-Algorithm/algorithm-problem/baekjoon-2484.py another solution

--- DataStructure-Algorithm/algorithm-problem/baekjoon-2484.py
+++ DataStructure-Algorithm/algorithm-problem/baekjoon-2484.py
@@ -24,3 +24,17 @@ def solution():
         else:
             prizes.append(max([int(i) for i in num]) * 100)
     return max(prizes)
+
+def money():
+    lst = sorted(list(map(int, input().split())))
+    if len(set(lst)) == 1:
+        return lst[0] * 5000 + 50000
+    if len(set(lst)) == 2:
+        if lst[1] == lst[2]: return lst[1] * 1000 + 10000
+        return lst[1] * 500 + lst[2] * 500 + 2000
+    for i in range(3):
+        if lst[i] == lst[i+1]: return 1000 + lst[i] * 100
+    return lst[-1] * 100
+N = int(input())
+print(max(money() for i in range(N)))
+
