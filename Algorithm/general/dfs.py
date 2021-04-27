9a854702552e89a7459f2bf140b11b807a118e36
변웅섭 <woungsub1234@gmail.com>
2021-04-27 20:50:39 +0900

    add new dfs algo

--- Algorithm/general/dfs.py
+++ Algorithm/general/dfs.py
@@ -21,3 +21,15 @@ graph = [
 visited = [False] * 9
 # 최상단 노드 1
 dfs(graph, 1, visited)
+
+# new dfs code
+def dfs(graph, start_node):
+    visited, need_visit = list(), list()
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop()
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+    return visited
